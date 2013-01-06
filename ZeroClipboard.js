6655292d9dac771bf0bd0ad231afd548bc268a3d
Jon Rohan <rohan@github.com>
2013-01-06 09:18:59 -0800

    moving a bunch around to dom and private methods

--- ZeroClipboard.js
+++ ZeroClipboard.js
@@ -174,6 +174,29 @@
     }
     return element;
   };
+  var _getDOMObjectPosition = function(obj) {
+    var info = {
+      left: 0,
+      top: 0,
+      width: obj.width || obj.offsetWidth || 0,
+      height: obj.height || obj.offsetHeight || 0,
+      zIndex: 9999
+    };
+    var zi = _getStyle(obj, "zIndex");
+    if (zi && zi != "auto") {
+      info.zIndex = parseInt(zi, 10);
+    }
+    while (obj) {
+      var borderLeftWidth = parseInt(_getStyle(obj, "borderLeftWidth"), 10);
+      var borderTopWidth = parseInt(_getStyle(obj, "borderTopWidth"), 10);
+      info.left += isNaN(obj.offsetLeft) ? 0 : obj.offsetLeft;
+      info.left += isNaN(borderLeftWidth) ? 0 : borderLeftWidth;
+      info.top += isNaN(obj.offsetTop) ? 0 : obj.offsetTop;
+      info.top += isNaN(borderTopWidth) ? 0 : borderTopWidth;
+      obj = obj.offsetParent;
+    }
+    return info;
+  };
   ZeroClipboard.dispatch = function(eventName, args) {
     ZeroClipboard._client.receiveEvent(eventName, args);
   };
@@ -286,29 +309,6 @@
     } else if (element.detachEvent) {
       element.detachEvent("on" + method, func);
     }
-  };
-  var _getDOMObjectPosition = function(obj) {
-    var info = {
-      left: 0,
-      top: 0,
-      width: obj.width || obj.offsetWidth || 0,
-      height: obj.height || obj.offsetHeight || 0,
-      zIndex: 9999
-    };
-    var zi = _getStyle(obj, "zIndex");
-    if (zi && zi != "auto") {
-      info.zIndex = parseInt(zi, 10);
-    }
-    while (obj) {
-      var borderLeftWidth = parseInt(_getStyle(obj, "borderLeftWidth"), 10);
-      var borderTopWidth = parseInt(_getStyle(obj, "borderTopWidth"), 10);
-      info.left += isNaN(obj.offsetLeft) ? 0 : obj.offsetLeft;
-      info.left += isNaN(borderLeftWidth) ? 0 : borderLeftWidth;
-      info.top += isNaN(obj.offsetTop) ? 0 : obj.offsetTop;
-      info.top += isNaN(borderTopWidth) ? 0 : borderTopWidth;
-      obj = obj.offsetParent;
-    }
-    return info;
   };
   if (typeof module !== "undefined") {
     module.exports = ZeroClipboard;
--- test/dom.js
+++ test/dom.js
@@ -2,6 +2,13 @@
 
 require("./env")
 var zeroClipboard, clip;
+var sandbox = require('nodeunit').utils.sandbox;
+var _dom = sandbox("./src/javascript/ZeroClipboard/dom.js", {
+  window: window,
+  document: document,
+  navigator: navigator
+});
+
 exports.domtests = {
 
   setUp: function (callback) {
@@ -15,80 +22,90 @@ exports.domtests = {
     callback();
   },
 
-  "Object has a title": function (test) {
+  "_removeClass removes classes from element": function (test) {
+    var div = $("<div>").addClass("class-1 class-2 class_3")[0];
+
+    _dom._removeClass(div, "class-1");
+    test.equal(div.className, "class-2 class_3");
+
+    _dom._removeClass(div, "classd");
+    test.equal(div.className, "class-2 class_3");
+
+    _dom._removeClass(div, "class-2");
+    test.equal(div.className, "class_3");
+
+    _dom._removeClass(div, "class_3");
+    test.equal(div.className, "");
+
+    _dom._removeClass(div, "class-3");
+    test.equal(div.className, "");
+
+    test.done();
+  },
+
+  "_removeClass doesn't remove partial class names": function (test) {
+    var div = $("<div>").addClass("class-1 class-2 class_3")[0];
+
+    _dom._removeClass(div, "ass");
+    test.equal(div.className, "class-1 class-2 class_3");
+
+    _dom._removeClass(div, "-");
+    test.equal(div.className, "class-1 class-2 class_3");
+
+    _dom._removeClass(div, " ");
+    test.equal(div.className, "class-1 class-2 class_3");
+
+    test.done();
+  },
+
+  "_addClass adds a class name": function (test) {
+    var div = $("<div>")[0];
+
+    _dom._addClass(div, "class-1");
+    test.equal(div.className, "class-1");
+
+    _dom._addClass(div, "class-2");
+    test.equal(div.className, "class-1 class-2");
+
+    _dom._addClass(div, "class-3");
+    test.equal(div.className, "class-1 class-2 class-3");
+
+    _dom._addClass(div, "class-3");
+    test.equal(div.className, "class-1 class-2 class-3");
+
+    test.done();
+  },
+
+  "elements with addClass already use the function": function (test) {
+    var div = $("<div>");
+
+    _dom._addClass(div, "class-1");
+    test.equal(div[0].className, "class-1");
+
+    test.done();
+  },
+
+  "elements with removeClass already use the function": function (test) {
+    var div = $("<div>").addClass("class-1");
+
+    test.equal(div[0].className, "class-1");
+
+    _dom._removeClass(div, "class-1");
+    test.equal(div[0].className, "");
+
+    test.done();
+  },
+
+  "when object borderWidth isNaN don't fail": function (test) {
 
     clip.glue($("#d_clip_button"));
 
-    var element = $("#d_clip_button")[0];
+    clip.setCurrent($("#d_clip_button")[0]);
 
-    clip.setCurrent(element);
-
-    test.equal(clip.htmlBridge.getAttribute("title"), "Click me to copy to clipboard.")
-
-    clip.resetBridge();
-
-    test.done();
-  },
-
-  "Object has no title": function (test) {
-
-    clip.glue($("#d_clip_button_no_title"));
-
-    var element = $("#d_clip_button_no_title")[0];
-
-    clip.setCurrent(element);
-
-    test.ok(!clip.htmlBridge.getAttribute("title"));
-
-    test.done();
-  },
-
-  "Object has data-clipboard-text": function (test) {
-
-    clip.glue($("#d_clip_button"));
-    var element = $("#d_clip_button")[0];
-
-    clip.setCurrent(element);
-
-    test.equal(clip._text, "Copy me!")
-
-    clip.resetBridge();
-
-    test.done();
-  },
-
-  "Object doesn't have data-clipboard-text": function (test) {
-
-    clip.glue($("#d_clip_button_no_text"));
-    var element = $("#d_clip_button_no_text")[0];
-
-    clip.setCurrent(element);
-
-    test.ok(!clip.htmlBridge.getAttribute("data-clipboard-text"));
-
-    test.done();
-  },
-
-  "Bridge is ready": function (test) {
-
-    zeroClipboard.dispatch("load", { flashVersion: "MAC 11,0,0" });
-
-    test.ok(clip.ready());
-
-    test.done();
-  },
-
-  "Trying a new client is same client": function (test) {
-
-    test.ok(clip.htmlBridge);
-
-    test.equal($(".global-zeroclipboard-container").length, 1);
-
-    var clip2 = new zeroClipboard.Client();
-
-    test.equal($(".global-zeroclipboard-container").length, 1);
-
-    test.equal(clip2, clip);
+    test.equal(clip.htmlBridge.style.top, "0px");
+    test.equal(clip.htmlBridge.style.left, "0px");
+    test.equal(clip.htmlBridge.style.width, "0px");
+    test.equal(clip.htmlBridge.style.height, "0px");
 
     test.done();
   }
