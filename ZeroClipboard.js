ce22dd148e40427422baecfc4cdd27c0faeec2a8
James M. Greene <james.m.greene@gmail.com>
2014-04-21 15:04:19 -0500

    v2.0.0-beta.4

--- ZeroClipboard.js
+++ ZeroClipboard.js
@@ -4,7 +4,7 @@
 * Copyright (c) 2014 Jon Rohan, James M. Greene
 * Licensed MIT
 * http://zeroclipboard.org/
-* v2.0.0-beta.3
+* v2.0.0-beta.4
 */
 (function(window) {
   "use strict";
@@ -678,7 +678,7 @@
     }
     return clients;
   };
-  ZeroClipboard.version = "2.0.0-beta.3";
+  ZeroClipboard.version = "2.0.0-beta.4";
   var _globalConfig = {
     swfPath: _swfPath,
     trustedDomains: window.location.host ? [ window.location.host ] : [],
