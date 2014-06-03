78a32a79235110a0892581a060655f08be3f78aa
James M. Greene <james.m.greene@gmail.com>
2014-06-03 10:44:07 -0500

    v2.0.0

--- dist/ZeroClipboard.Core.js
+++ dist/ZeroClipboard.Core.js
@@ -4,7 +4,7 @@
  * Copyright (c) 2014 Jon Rohan, James M. Greene
  * Licensed MIT
  * http://zeroclipboard.org/
- * v2.0.0-beta.8
+ * v2.0.0
  */
 (function(window, undefined) {
   "use strict";
@@ -1600,7 +1600,7 @@
  * @readonly
  * @property {string}
  */
-  ZeroClipboard.version = "2.0.0-beta.8";
+  ZeroClipboard.version = "2.0.0";
   _makeReadOnly(ZeroClipboard, "version");
   /**
  * Update or get a copy of the ZeroClipboard global configuration.
